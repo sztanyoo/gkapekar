4557b3f52e22ea9c0fdb114a4b430da95cbbb429
Bence Sztankovszky <sztankovszky.bence@gmail.com>
2019-11-25 23:30:37 +0100

    Implement is_kaprekar properly

--- kapekar.py
+++ kapekar.py
@@ -1,31 +1,26 @@
 import unittest
 
 def is_kaprekar(n):
-    if not n:
-        return True
-    if n<=3 and n**2==0+n:
-        return True
-    else:
-        return False
-    if n>3 and n**2%2==0:
-        if int(str(n**2).split()[0])+int(str((n**2).split()[1]))==n**2:
+    nSquare = n*n
+    for i in range(len(str(nSquare))):
+        left = 0
+        if 0 < i:
+            left = str(nSquare)[0:i]
+        right = str(nSquare)[i:]
+        if int(left) + int(right) == n:
             return True
-        else:
-            return False
-    if n>3 and (n**2)%2!=0:
-        if int(str((n**2)[:0]))+int(str(n**2)[0:])==n**2:
-            return True
-        else:
-            return False
-
+    return False
 
 class testKapekar(unittest.TestCase):
     def testKapekars(self):
         self.assertTrue(is_kaprekar(0))
         self.assertTrue(is_kaprekar(1))
         self.assertFalse(is_kaprekar(2))
+        self.assertFalse(is_kaprekar(5))
+        self.assertFalse(is_kaprekar(65))
+        self.assertTrue(is_kaprekar(99))
+        self.assertTrue(is_kaprekar(297))
 
-        # Add your further cases here
 
 def main():
     unittest.main(verbosity=2)
